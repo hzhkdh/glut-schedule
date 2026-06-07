9e04204ef603faba89bcf0a5bb5a3c9430b0db55
hzhkdh <3380564007@qq.com>
2026-06-08 01:41:15 +0800

    chore: bump version to 0.7.13 (versionCode 47)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.glut.schedule"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "0.7.12"
+        versionCode = 47
+        versionName = "0.7.13"
 
     }
 
